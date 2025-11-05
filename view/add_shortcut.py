d4bd31102eda6709ea617b424197fb305aaabf09
Z47 <zhoushiqi@sepflow.com>
2025-11-05 10:36:05 +0800

    添加快捷键map

--- view/add_shortcut.py
+++ view/add_shortcut.py
@@ -1,11 +1,16 @@
 import math
 import sys
 from PyQt5.QtWidgets import QDialog, QMessageBox
-from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtCore import Qt, pyqtSignal, QEvent
+from PyQt5.QtGui import QKeySequence
 
 
 from ui.add_shortcut import Ui_AddShortcut
 from utils import Database
+
+
+
+# TODO 当我焦点在le_shortcut的时候，我在键盘上无论按下什么键，都用一个函数把它显示出来
 
 
 class AddShortcutView(QDialog, Ui_AddShortcut):
@@ -24,6 +29,10 @@ class AddShortcutView(QDialog, Ui_AddShortcut):
         
         # 连接按钮信号
         self.btn_add.clicked.connect(self.handle_add)
+
+        # 为快捷键输入框安装事件过滤器以捕获键盘输入
+        self.le_shortcut.installEventFilter(self)
+
         
 
     def __init_show(self):
@@ -67,3 +76,35 @@ class AddShortcutView(QDialog, Ui_AddShortcut):
         })
 
         self.close()
+
+    # --- 键盘捕获与显示 ---
+    def eventFilter(self, obj, event):
+        # 当焦点在 le_shortcut 上时，捕获键盘事件
+        if obj is self.le_shortcut and event.type() == QEvent.KeyPress:
+            return self._handle_shortcut_key_press(event)
+        return super(AddShortcutView, self).eventFilter(obj, event)
+
+    def _handle_shortcut_key_press(self, event):
+        key = event.key()
+        mods = event.modifiers()
+
+        # 清空逻辑
+        if key in (Qt.Key_Backspace, Qt.Key_Delete, Qt.Key_Escape):
+            self.le_shortcut.clear()
+            return True
+
+        # 过滤掉仅修饰键（不生成序列）
+        if key in (Qt.Key_Control, Qt.Key_Shift, Qt.Key_Alt, Qt.Key_Meta, Qt.Key_Super_L, Qt.Key_Super_R):
+            return True
+
+        # 合成 QKeySequence（PyQt5 需要把修饰键与主键进行或运算）
+        combined = int(mods) | int(key)
+        sequence = QKeySequence(combined)
+
+        # 将本地化显示文本写入输入框
+        text = sequence.toString(QKeySequence.NativeText)
+        # 若生成结果为空（极少见），则不修改
+        if text:
+            self.le_shortcut.setText(text)
+
+        return True
